6b9ac3e531c19147de66ab6aadc18cad738e4d04
Firouzfar <firouzfar.sebastien@gmail.com>
2022-04-04 20:01:46 +0200

    Design the website and add the new price toggle

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var toggler = document.querySelector('.toggle-switch');
+var toggler = document.querySelector(".toggle-switch");
+var price = document.querySelectorAll(".price");
 
-toggler.onclick = function(){
-  toggler.classList.toggle('active');
-}
+toggler.onclick = function () {
+  toggler.classList.toggle("active");
+  if (toggler.classList.contains("active")) {
+    for (var i = 0; i < price.length; i++) {
+      price[i].innerText = price[i].innerText * 10;
+    }
+  } else {
+    for (var j = 0; j < price.length; j++) {
+      price[j].innerText = price[j].innerText / 10;
+    }
+  }
+};
