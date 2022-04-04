eccf5ef0ef7b43cf3edff2912c48c273421bea17
Firouzfar <firouzfar.sebastien@gmail.com>
2022-04-04 21:23:45 +0200

    Change the price with javascript

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var toggler = document.querySelector(".toggle-switch");
-var price = document.querySelectorAll(".price");
+let toggler = document.querySelector(".toggle-switch");
+let price = document.querySelectorAll(".price");
 
-toggler.onclick = function () {
+toggler.onclick = () => {
   toggler.classList.toggle("active");
   if (toggler.classList.contains("active")) {
     for (var i = 0; i < price.length; i++) {
-      price[i].innerText = price[i].innerText * 10;
+      price[i].innerText = price[i].innerText / 10;
     }
   } else {
-    for (var j = 0; j < price.length; j++) {
-      price[j].innerText = price[j].innerText / 10;
+    for (var i = 0; i < price.length; i++) {
+        price[i].innerText = price[i].innerText * 10;
     }
   }
 };
