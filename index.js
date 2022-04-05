209714339945bee771b4248e21ae6609ff70c99e
Firouzfar <firouzfar.sebastien@gmail.com>
2022-04-05 17:47:00 +0200

    Update the design

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ toggler.onclick = () => {
   toggler.classList.toggle("active");
   if (toggler.classList.contains("active")) {
     for (var i = 0; i < price.length; i++) {
-      price[i].innerText = price[i].innerText / 10;
+      price[i].innerText =  price[i].innerText / 10;
     }
   } else {
     for (var i = 0; i < price.length; i++) {
